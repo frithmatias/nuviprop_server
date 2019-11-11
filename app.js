3b3accf2174dca5540c25205627c065ac14d06e5
Matias Frith <frithmatias@gmail.com>
2019-11-10 22:22:57 -0300

    inMob Server, primer commit

---
+++ app.js
@@ -0,0 +1,84 @@
+// ==================================================
+// Requires
+// ==================================================
+var express = require('express');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+
+// ==================================================
+// Inicializar variables
+// ==================================================
+var app = express();
+
+// ==================================================
+// MIDDLEWARE CORS
+// ==================================================
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS");
+    next();
+  });
+
+// .USE() es un MIDDLEWARE, cualquier petición que llegue primero va a pasar por esta función.
+// Body Parser 
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+// parse application/json
+app.use(bodyParser.json());
+
+// ==================================================
+// Importar Rutas 
+// ==================================================
+var loginRoutes = require('./routes/login.routes');
+var userRoutes = require('./routes/user.routes');
+// var propiedadRoutes = require('./routes/propiedad');
+// var busquedaRoutes = require('./routes/busqueda');
+// var uploadRoutes = require('./routes/upload');
+// var imagenesRoutes = require('./routes/imagenes');
+// var appRoutes = require('./routes/app');
+
+// ==================================================
+// Asignar Rutas
+// ==================================================
+app.use('/login', loginRoutes);
+app.use('/user', userRoutes);
+// app.use('/propiedad', propiedadRoutes);
+// app.use('/busqueda', busquedaRoutes);
+// app.use('/upload', uploadRoutes);
+// app.use('/imagenes', imagenesRoutes);
+// app.use('/', appRoutes);
+
+// ==================================================
+// File Server index config
+// ==================================================
+// var serveIndex = require('serve-index');
+// para poder ver las imagenes, si comento esta linea me devuelve Cannot GET /uploads/hospitales/5c68069aa4ab291fc4a0f5af-516.jpg
+// app.use(express.static(__dirname + '/')) 
+// para poder navegar carpetas dentro de /uploads
+// app.use('/uploads', serveIndex(__dirname + '/uploads'));
+
+
+// ==================================================
+// Conexión a la base de datos.
+// ==================================================
+mongoose.connection.openUri('***REMOVED***', (err, res) => {
+
+if (err) throw err;
+console.log('MongoDB corriendo en el puerto 27017: \x1b[32m%s\x1b[0m', 'ONLINE');
+
+} ); // Segundo argumento es una función de callback.
+
+
+// Escuchar peticiones 
+app.listen(3000, () => {
+    console.log('Express Server corriendo en el puerto 3000: \x1b[32m%s\x1b[0m', 'ONLINE');
+});
+
+/*
+Que es lo mismo que 
+app.listen(3000, function(){
+
+})
+*/
---
+++ middlewares/auth.middleware.js
@@ -0,0 +1,58 @@
+var jwt = require('jsonwebtoken');
+var SEED = require('../config/config').SEED;
+
+// ==================================================
+// Verificar TOKEN 
+// ==================================================
+
+exports.verificaToken = function(req, res, next) {
+
+    // En la app de Ionic 4, de fotosGram, en el backend usa el metodo get para obtener el nombre 
+    // de la propiedad en la que viene el token. 
+    // const userToken = req.get('x-token' || '');
+    // para mas info 
+    // https://expressjs.com/es/api.html#req
+
+    // req.query.token obtiene el token como parametro en la URL en POSTMAN lo envio en PARAMS y 
+    // le concatena el token=al2afj23rñasfdk... en la URL pero yo no quiero enviarlo en la URL, 
+    // voy a enviarlo en los Headers como 'x-token'
+
+    //var token = req.query.token;
+    const userToken = req.get('x-token' || '');
+    console.log('REQ.QUERY: ' , req.query);
+    jwt.verify(userToken, SEED, (err, decoded) => {
+        if (err) {
+            return res.status(401).json({ //401 UNAUTHORIZED
+                ok: false,
+                mensaje: 'Token Incorrecto',
+                errors: err
+            });
+        }
+        // usuario QUE HIZO la SOLICITUD, en el request.
+        req.usuario = decoded.usuario;
+        next();
+    });
+};
+
+// ==================================================
+// Verificar si tiene privilegios de ADMIN 
+// ==================================================
+
+exports.canUpdate = function(req, res, next) {
+    console.log('VERIFICANDO SI ES ADMIN...');
+    // Aca no vamos a usar el TOKEN porque yo ya se desde el middleware de verificaToken que el token ES valido.
+    var user_request = req.usuario;
+    var user_to_update = req.params.id; // -> app.put('/:id',
+    if ( user_request.role === 'ADMIN_ROLE' || user_request._id === user_to_update) {
+        console.log('Accion permitida, el usuario es role Admin, o apunta al mismo usuario.');
+        next();
+        return;
+    } else {
+        console.log('EL USUARIO ES ROLE USER...');
+            return res.status(401).json({ //401 UNAUTHORIZED
+                ok: false,
+                mensaje: 'Token Incorrecto - el role no es ADMIN_ROLE',
+                errors: {message: 'No puede ejecutar la accion solicitada, no posee permisos de administrador.'}
+            });
+    }
+};
